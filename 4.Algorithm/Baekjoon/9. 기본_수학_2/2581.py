c0fa175bfa70018a36c542613e40c219b4246ab9
dayeonyang0324 <allkites324@gmail.com>
2021-06-23 16:13:55 +0900

    기본수학2

---
+++ 4.Algorithm/Baekjoon/9. 기본_수학_2/2581.py
@@ -0,0 +1,26 @@
+# 2581
+
+# 1978과 유사한 문제
+mini = int(input())
+maxi = int(input())
+
+# 모든 소수를 출력하기 위해 리스트를 만든다
+result = []
+for num in range(mini, maxi+1):
+    cnt = 0
+    # 1보다 큰 수여야 소수의 조건 만족한다
+    # 나머지는 1978번과 유사
+    if num > 1:
+        for i in range(2, num):
+            if num % i == 0:
+                cnt += 1
+                break  # break로 for문을 끝내줘야 시간초과가 뜨지 않음
+        if cnt == 0:
+            result.append(num)
+
+# if len(result) == 0일떄로 하면 런타임 에러 발생
+if len(result) > 0:
+    print(sum(result))
+    print(min(result))
+else:
+    print(-1)
