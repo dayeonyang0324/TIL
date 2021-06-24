23c7ad30fdac4624f3511a75e860e4f448599dd4
dayeonyang0324 <allkites324@gmail.com>
2021-06-24 18:21:55 +0900

    베르트랑 공준

--- 4.Algorithm/Baekjoon/9. 기본_수학_2/4948.py
+++ 4.Algorithm/Baekjoon/9. 기본_수학_2/4948.py
@@ -1,25 +1,26 @@
 # 4948
 
 # 1929 문제와 유사하게 풀었지만 시간초과
-def prime(num):
-    for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
+# def prime(num):
+#     for i in range(2, int(num ** 0.5) + 1):
+#         if num % i == 0:
+#             return False
+#     return True
+#
+#
+# while True:
+#     N = int(input())
+#     cnt = 0
+#     for num in range(N+1, 2*N + 1):
+#         if prime(num):
+#             cnt += 1
+#     if N == 0:
+#         break
+#     print(cnt)
 
-
-while True:
-    N = int(input())
-    cnt = 0
-    for num in range(N+1, 2*N + 1):
-        if prime(num):
-            cnt += 1
-    if N == 0:
-        break
-    print(cnt)
 
 # 베르트랑 공준
-# 체크하기 위한 리스트 만들어서 제곱근들을 확인하기
+# 체크하기 위한 리스트 만들어서 제곱근들만 확인하기
 N = 123456 * 2 + 1
 check = [True] * N
 for i in range(2, int(N**0.5)+1):
