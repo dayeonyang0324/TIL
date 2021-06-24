b4ef2ea49d6b669b1a0dcb6dcc09162a4fef5d43
dayeonyang0324 <allkites324@gmail.com>
2021-06-24 18:19:45 +0900

    네번째 점

--- 4.Algorithm/Baekjoon/9. 기본_수학_2/3009.py
+++ 4.Algorithm/Baekjoon/9. 기본_수학_2/3009.py
@@ -1,10 +1,11 @@
 # 3009
+
 # 주어진 좌표들을 정리
 x1, y1 = map(int, input().split())
 x2, y2 = map(int, input().split())
 x3, y3 = map(int, input().split())
 
-# x 좌표에서 같은 값이 2개, 1개 주어졌을 때 나머지 1개가 최종값이 되어야한다
+# x 좌표에서 같은 값이 2개, 1개 주어졌을 때 1개만 있는 값이 최종값이 되어야한다
 # 리스트에 넣으면서 값이 있으면 빼면 최종적으로 남는 1개가 답이 된다
 x = [x1, x2, x3]
 result_x = []
@@ -14,7 +15,7 @@ for i in x:
     else:
         result_x.append(i)
 
-# x축과 마찬가지로 y값도 나머지 한개가 최종값이 된다
+# x축과 마찬가지로 y값도 1개만 있는 값이 최종값이 된다
 y = [y1, y2, y3]
 result_y = []
 for j in y:
