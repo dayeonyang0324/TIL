0a0df09fc04f120f9943a5930131cde775be0ea1
dayeonyang0324 <allkites324@gmail.com>
2021-07-06 00:30:50 +0900

    덩치

--- 4.Algorithm/Baekjoon/11. 브루트_포스/7568.py
+++ 4.Algorithm/Baekjoon/11. 브루트_포스/7568.py
@@ -1,18 +1,17 @@
-import sys
-sys.stdin = open('input.txt')
+# 7568
 
+# 사람들의 몸무게와 키 리스트
 N = int(input())
 people = []
 for i in range(N):
     x, y = map(int, input().split())
     people.append([x, y])
 
-
-# 사람들의 몸무게와 키를 비교해야함
 result = []
 for i in range(N):
     cnt = 1
     for j in range(N):
+        # 몸무게, 키가 둘다 크지 않다면 나보다 덩치 큰 사람 다음이 내 등수가 된다
         if people[i][0] < people[j][0] and people[i][1] < people[j][1]:
             cnt += 1
     result.append(cnt)
