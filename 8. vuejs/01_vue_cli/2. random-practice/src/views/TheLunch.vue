853ee91619171850122cd043d2c992425a0a3222
dayeonyang0324 <allkites324@gmail.com>
2021-07-08 23:11:18 +0900

    메뉴추천 및 로또번호 랜덤 추첨 연습

---
+++ 8. vuejs/01_vue_cli/2. random-practice/src/views/TheLunch.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2>점심메뉴 추천</h2>
+    <button @click="pickOneLunchMenu">메뉴추천!</button>
+    <p>오늘의 점심 메뉴 : {{ selectedLunchMenu }}</p>
+  </div>
+</template>
+
+<script>
+import _ from 'lodash'
+
+export default {
+  name: 'TheLunch',
+  data: function () {
+    return {
+      lunch: ['보쌈', '라면', '곱창', '떡볶이'],
+      selectedLunchMenu: '',
+    }
+  },
+  methods: {
+    pickOneLunchMenu: function () {
+      this.selectedLunchMenu = _.sample(this.lunch)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
