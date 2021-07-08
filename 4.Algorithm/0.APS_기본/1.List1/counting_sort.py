da6177d91aac890ef7e25df6e5aeef94b2598651
dayeonyang0324 <allkites324@gmail.com>
2021-07-09 01:20:36 +0900

    카운트 정렬

--- 4.Algorithm/0.APS_기본/1.List1/counting_sort.py
+++ 4.Algorithm/0.APS_기본/1.List1/counting_sort.py
@@ -4,22 +4,23 @@
 # count = [0] * (N)
 
 
-def Counting_sort(A, B, k):
-    # A[] # 입력배열
-    # B[] # 정렬된 배열
-    # C[] # 카운트 배열
+def counting(arr, N):
+    count_arr = [0] * (N+1)  # 빈도 수를 셀 리스트 만들기
 
-    C = [0] * k
+    # 빈도수 세기
+    for i in arr:
+        count_arr[i] += 1
 
-    for i in range(0, len(B)):
-        C[A[i]] += 1
+    # 직전값 누적시켜 빈도수 업데이트
+    for i in range(N):
+        count_arr[i+1] += count_arr[i]
 
-    for i in range(1, len(C)):
-        C[i] += C[i+1]
+    # 결과 초기값 리스트
+    result = [-1] * len(arr)
 
-    for i in range(len(B)-1, -1, -1):
-        B[C[A[i]] - 1] = A[i]
-        C[A[i]] -= 1
+    # 역순으로 순회를 돌면서 결과값을 채워넣고 빈도수를 감소시킨다
+    for i in arr:
+        result[count_arr[i] - 1] = i
+        count_arr[i] -= 1
 
-
-
+    return result
